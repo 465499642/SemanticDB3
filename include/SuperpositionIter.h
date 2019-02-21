05076c7cf5a9cf60e9b03e21a0c272ab3ffa8232
Garry Morrison <garry@semantic-db.org>
2019-02-21 12:58:12 +1030

    first step towards making SuperpositionIter a random access iterator

--- include/SuperpositionIter.h
+++ include/SuperpositionIter.h
@@ -6,18 +6,44 @@
 class Superposition;
 class Ket;
 
-class SuperpositionIter{
+class SuperpositionIter : public std::iterator<std::random_access_iterator_tag, Superposition> {
     private:
-        ulong _idx;
+        ulong _pos;
         const Superposition *_p_sp;
 
     public:
-        SuperpositionIter(const Superposition* p_sp, ulong idx);
-        bool operator!= (const SuperpositionIter& other) const;
-        Ket operator* () const;
-        const SuperpositionIter& operator++ ();
+        using difference_type = typename std::iterator<std::random_access_iterator_tag, Superposition>::difference_type;
 
-        SuperpositionIter& operator- (const SuperpositionIter& other);
+        SuperpositionIter(const Superposition* p_sp, ulong pos) { _pos = pos; _p_sp = p_sp; }
+
+        SuperpositionIter& operator+=(difference_type rhs) { _pos += rhs; return *this; }
+        SuperpositionIter& operator-=(difference_type rhs) { _pos -= rhs; return *this; }
+        // Ket operator* () const { return _p_sp->get(_pos); }
+        // Type* operator->() const;
+        // Ket operator[](difference_type rhs) const { return _p_sp->get(rhs); }
+        Ket operator* () const;
+        Ket operator[](difference_type rhs) const;
+
+        SuperpositionIter& operator++() { ++_pos; return *this; }
+        SuperpositionIter& operator--() { --_pos; return *this; }
+        SuperpositionIter operator++(int) { SuperpositionIter tmp(_p_sp, _pos); ++_pos; return tmp; }
+        SuperpositionIter operator--(int) { SuperpositionIter tmp(_p_sp, _pos); --_pos; return tmp; }
+
+        difference_type operator-(const SuperpositionIter& rhs) const { return _pos - rhs._pos; }
+        SuperpositionIter operator+(difference_type rhs) const { return SuperpositionIter(_p_sp, _pos + rhs); }
+        SuperpositionIter operator-(difference_type rhs) const { return SuperpositionIter(_p_sp, _pos - rhs); }
+        friend SuperpositionIter operator+(difference_type lhs, const SuperpositionIter& rhs) { return SuperpositionIter(rhs._p_sp, lhs + rhs._pos); }
+        friend SuperpositionIter operator-(difference_type lhs, const SuperpositionIter& rhs) { return SuperpositionIter(rhs._p_sp, lhs - rhs._pos); }
+
+        bool operator== (const SuperpositionIter& rhs) const { return _pos == rhs._pos; }
+        bool operator!= (const SuperpositionIter& rhs) const { return _pos != rhs._pos; }
+        bool operator> (const SuperpositionIter& rhs) const { return _pos > rhs._pos; }
+        bool operator< (const SuperpositionIter& rhs) const { return _pos < rhs._pos; }
+        bool operator>= (const SuperpositionIter& rhs) const { return _pos >= rhs._pos; }
+        bool operator<= (const SuperpositionIter& rhs) const { return _pos <= rhs._pos; }
+
+        // const SuperpositionIter& operator++ ();
+        // SuperpositionIter& operator- (const SuperpositionIter& other);
 };
 
 #endif
